d6f223190feede7f0a0580c0c013b8c24d109f3e
parkerhams <parker.hamilton@loop.colum.edu>
2018-05-10 18:17:33 -0500

    Working on Audio Scene

--- Assets/Scripts/EerieAudio.cs
+++ Assets/Scripts/EerieAudio.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EerieAudio : MonoBehaviour {
+    //Get ready for CoRoutines: The Script
+
+    [SerializeField]
+    AudioSource overallMusic;
+
+    [SerializeField]
+    AudioSource explosion;
+
+    [SerializeField]
+    AudioSource electric;
+
+    [SerializeField]
+    AudioSource orchestra;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        overallMusic.Play();
+        StartCoroutine(playTheElectric());
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	private IEnumerator playTheElectric()
+    {
+        yield return new WaitForSeconds(4f);
+        electric.Play();
+        StartCoroutine(playTheOrchestra());
+    }
+
+    private IEnumerator playTheOrchestra()
+    {
+        yield return new WaitForSeconds(3f);
+        orchestra.Play();
+    }
 }
